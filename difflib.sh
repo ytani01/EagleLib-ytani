31d9f61c6bd01ec5488d412ffb694f0c7330d1cb
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2022-11-11 20:22:44 +0900

    ADD: WS2812B

---
+++ difflib.sh
@@ -0,0 +1,34 @@
+#!/bin/sh 
+#
+# Copyright (c) 2022 Yoichi Tanibayashi
+#
+MYNAME=`basename $0`
+
+FNAME="ytani1.lbr"
+SRC_LIB="./lib/${FNAME}"
+DST_LIB="/mnt/c/Users/$USER/Documents/EAGLE/libraries/$FNAME"
+
+usage () {
+    echo "$MYNAME USER"
+}
+
+#
+# main
+#
+USER=$1
+if [ -z "$USER" ]; then
+    usage
+    exit 1
+fi
+
+if [ ! -f $SRC_LIB ]; then
+    echo "${SRC_LIB}: not found"
+    exit 1
+fi
+
+if [ ! -f $DST_LIB ]; then
+    echo "${DST_LIB}: not found"
+    exit 1
+fi
+
+diff -u ${SRC_LIB} ${DST_LIB}
